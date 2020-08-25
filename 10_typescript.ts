7f32c6b868091d4f27e0e13e94d5537d0e04d892
mpi <michal.piotrkowski@pragmatists.pl>
2020-08-25 13:07:57 +0200

    Create 10_typescript.ts

---
+++ 10_typescript.ts
@@ -0,0 +1,19 @@
+// type this:
+
+function toBoolean(value) {
+  if(value == "on") {
+     return true;
+  }
+  if(value == "off") {
+    return false;
+ }
+}
+
+
+// and this:
+
+const map = {
+  val1: 1,
+  arr2: [1],
+  fun3: () => 1
+};
